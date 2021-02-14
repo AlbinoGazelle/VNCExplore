6a4a81905355d9045e6b634ad4480abdd8d7be2f
burnsn3 <burnsn3@wwu.edu>
2021-02-14 01:15:28 -0800

    first complete version

--- vncexplore.py
+++ vncexplore.py
@@ -1,20 +1,39 @@
 import argparse
 import sys
 from shutil import which
+import subprocess
 
-def is_installed(name):
-    return which(name) is not None
-
-if(is_installed("vncsnapshot") == False):
+#check to ensure vncsnapshot is installed
+if ((which("vncsnapshot") is not None) == False):
     print("Missing vncsnapshot. Please install (apt-get install vncsnapshot)")
+    sys.exit()
 
-print(is_installed("vncsnapshot"))
+#parse arguments
 parser = argparse.ArgumentParser(description="Dump VNC Server Screenshots")
 parser.add_argument("target_list", help="List of IP Addresses to scan for VNC snapshots.")
 args = parser.parse_args()
+def get_screenshots():
+    try:
+        #get length of target list
+        lines = len(open(args.target_list).readlines())
+        #start counting variable
+        count = 0
+        #open target list for reading
+        f = open(args.target_list, "r")
+        for target in f:
+            #run vncsnapshot silently and check for errors
+            run_snapshot(target)
+            #increment count variable
+            count += 1
+            #print status
+            print(f"{count} out of {lines} completed ({(count / lines) * 100}%)")
+    except FileNotFoundError:
+        print("Cannot Find File!")
 
-test_server = "216.48.95.180"
-
-
-
-print(args.target_list)
+def run_snapshot(target):
+    try:
+        subprocess.check_call(["vncsnapshot", f"{target}", f"{target}.jpg"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print(f"Couldn't get screenshot for {target}")
+if __name__ == '__main__':
+    get_screenshots()
